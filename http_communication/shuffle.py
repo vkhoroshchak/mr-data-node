f100df7ee066754e95524fd14ec1d96357d066b0
mranch <anchiforov@gmail.com>
2021-11-19 21:39:08 +0200

    added try-except and traceback to shuffle.py

--- http_communication/shuffle.py
+++ http_communication/shuffle.py
@@ -1,11 +1,14 @@
+import dask.dataframe as dd
 import json
 import os
-
-import dask.dataframe as dd
 import pandas as pd
 import requests
+import traceback
 
+from config.logger import data_node_logger
 from receive_commands.receive_commands import Command
+
+logger = data_node_logger.get_logger(__name__)
 
 # uncomment for Ubuntu as it runs __file__ as ~
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
@@ -43,35 +46,39 @@ class ShuffleCommand:
 
 
 def shuffle(content):
-    file_id = content["file_id"]
-    full_file_path = os.path.join(Command.paths_per_file_name[file_id]["shuffle_folder_name_path"], 'shuffled.csv')
-    field_delimiter = content['field_delimiter']
+    try:
+        file_id = content["file_id"]
+        full_file_path = os.path.join(Command.paths_per_file_name[file_id]["shuffle_folder_name_path"], 'shuffled.csv')
+        field_delimiter = content['field_delimiter']
 
-    for f in Command.paths_per_file_name[file_id]["segment_list"]:
-        data_f = pd.read_parquet(os.path.join(Command.paths_per_file_name[file_id]["map_folder_name_path"],
-                                              f, "part.0.parquet"))
+        for f in Command.paths_per_file_name[file_id]["segment_list"]:
+            data_f = pd.read_parquet(os.path.join(Command.paths_per_file_name[file_id]["map_folder_name_path"],
+                                                  f, "part.0.parquet"))
 
-        # headers = list(data_f.columns)
+            # headers = list(data_f.columns)
 
-        for i in content['nodes_keys']:
-            index_list = []
-            for index, item in enumerate(data_f.loc[:, 'key_column']):
+            for i in content['nodes_keys']:
+                index_list = []
+                for index, item in enumerate(data_f.loc[:, 'key_column']):
 
-                min, max = i["hash_keys_range"]
-                last_node = max == content['max_hash']
-                hash_item = Command.hash_f(item)
-                hash_item_in_range = min <= hash_item < max
-                if hash_item_in_range or (hash_item == max and last_node):
-                    index_list.append(index)
+                    min, max = i["hash_keys_range"]
+                    last_node = max == content['max_hash']
+                    hash_item = Command.hash_f(item)
+                    hash_item_in_range = min <= hash_item < max
+                    if hash_item_in_range or (hash_item == max and last_node):
+                        index_list.append(index)
 
-            if i['data_node_ip'] == self_node_ip:
-                dd.from_pandas(data_f.iloc[index_list], npartitions=1).to_parquet(full_file_path,
-                                                                                  write_index=False,
-                                                                                  engine="pyarrow"
-                                                                                  )
-            else:
-                data = {'content': data_f.iloc[index_list].to_json(),
-                        'data_node_ip': i['data_node_ip']}
+                if i['data_node_ip'] == self_node_ip:
+                    dd.from_pandas(data_f.iloc[index_list], npartitions=1).to_parquet(full_file_path,
+                                                                                      write_index=False,
+                                                                                      engine="pyarrow"
+                                                                                      )
+                else:
+                    data = {'content': data_f.iloc[index_list].to_json(),
+                            'data_node_ip': i['data_node_ip']}
 
-                sc = ShuffleCommand(data, full_file_path, field_delimiter)
-                sc.send()
+                    sc = ShuffleCommand(data, full_file_path, field_delimiter)
+                    sc.send()
+    except Exception as e:
+        logger.info("Caught exception!" + e)
+        traceback.print_exc()
